2c392c151419ce69ccf6d99ddf7ee136043c6a09
Iarlen <iarlensilva775@gmail.com>
2017-09-05 11:53:44 -0300

    Simular comando do linux

--- src/pacote/App.java
+++ src/pacote/App.java
@@ -1,7 +1,32 @@
 package pacote;
 
-public class App {
-	public static void main(String[] args) {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+public class App {
+	private static final String entrada = "mozilla.pdf";
+	private static final String saida = "mozilla_copia.pdf";
+
+	public static void main(String[] args) throws IOException {
+		File arquivo = new File(entrada);
+		File arquivoSaida = new File(saida);
+		
+		InputStream is = new FileInputStream(arquivo);
+		OutputStream os = new FileOutputStream(arquivoSaida);
+		int b;
+		long inicial = System.currentTimeMillis();
+		while((b = is.read()) != -1){
+			os.write(b);
+		}
+		long t_final = System.currentTimeMillis() - inicial;
+		System.out.println("Tempo: " + t_final + " milissegundos");
+		is.close();
+		os.close();
 	}
+
+	
 }
